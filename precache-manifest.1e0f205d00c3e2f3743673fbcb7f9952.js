9a7b8d33ed5a14f8837a66ef88731a356272528f
lilman109 <akira.yoshidasjsu@gmail.com>
2020-09-22 15:28:35 +0900

    Updates

---
+++ precache-manifest.1e0f205d00c3e2f3743673fbcb7f9952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdffb78a82236fe75a00d87d4fdaf14d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5ec2e63016337777dde1",
+    "url": "/monsters-rolodex/static/css/main.97723463.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec2e63016337777dde1",
+    "url": "/monsters-rolodex/static/js/main.aaf3ef55.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
